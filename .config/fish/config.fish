8c4969f1020f781119c6f15386254e67be744627
ranebrown <rane.brown@gmail.com>
2016-12-11 08:55:12 -0700

    updates to fish config and functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 # functions that are loaded on start
 fish_vi_key_bindings
-my_key_bindings
+source ~/.config/fish/functions/my_vi_key_bindings.fish
 fish_user_key_bindings
+
+# environment variables
 set -x EDITOR vim
 set -x VISUAL vim
--- .config/fish/functions/my_key_bindings.fish
+++
@@ -1,5 +0,0 @@
-# map jj to exit vi insert mode
-function my_key_bindings
-	fish_vi_key_bindings
-    bind -M insert -m default jj force-repaint
-end
